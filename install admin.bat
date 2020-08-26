f3f6869b4ff24994d258094e212068a4773a6674
Tom <larsenthomasj@gmail.com>
2020-08-25 22:36:06 -0400

    quote file paths

--- install admin.bat
+++ install admin.bat
@@ -1,9 +1,9 @@
 SET currentFolder=%~dp0
 CD\
-CD Program Files\Alteryx\bin\plugins
-COPY %currentFolder%\go_tools.dll .\go_tools.dll /Y
-CD ..\RuntimeData\icons\categories
-COPY %currentFolder%\AyxBuilders.png .\AyxBuilders.png /Y
-%currentFolder%ayx_builders.yxi
-CD %currentFolder%
+CD "Program Files\Alteryx\bin\plugins"
+COPY "%currentFolder%go_tools.dll" ".\go_tools.dll" /Y
+CD "..\RuntimeData\icons\categories"
+COPY "%currentFolder%AyxBuilders.png" ".\AyxBuilders.png" /Y
+"%currentFolder%ayx_builders.yxi"
+CD "%currentFolder%"
 PAUSE
